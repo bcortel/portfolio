e1c83e4c464b163ea64ea9658c7a8f5dedc1d820
bcortel <cortel.butch.kleina@gmail.com>
2021-10-16 22:11:43 +0800

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,12 @@ module.exports = {
   purge: [
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/layout/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
+  variants: { extend: {} },
+  plugins: [],
 };
